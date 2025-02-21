e1f8e1204721c7405d3fcd7e5e42b825225a34df
smfabiann <smfabiann@gmail.com>
2025-02-21 18:50:44 -0300

    pene

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,53 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
 
 constexpr unsigned int WIDTH = 1600u;
 constexpr unsigned int HEIGHT = 900u;
+const unsigned int FPS = 60;
+
+class BolitaConGravedad : public sf::CircleShape {
+private:
+    const int masa = 1;
+    const sf::Vector2f aceleracion = {0, 1};
+    sf::Vector2f velocidad = {0, 0};
+    /*sf::Vector2f peso = aceleracion();*/
+public:
+    BolitaConGravedad(float diametro) : sf::CircleShape(diametro) {};
+    
+    /* gets */
+    sf::Vector2f getVelocidad() { return velocidad; }
+    int getMasa() { return masa; }
+
+    /* sets */
+    void setVelocidad(sf::Vector2f v) { velocidad = v; };
+
+    void update() {
+        if (getPosition().y > HEIGHT - getRadius()) {
+            velocidad = {-velocidad.x, -velocidad.y};
+            setPosition({ getPosition().x, HEIGHT - getRadius() });
+        }
+        velocidad += aceleracion;
+        setPosition(getPosition() + velocidad);
+    }
+};
+
+BolitaConGravedad spawnearBolita(const sf::Vector2f& position) {
+    const float tamano = 26.0f;
+    BolitaConGravedad circulo(tamano);
+    circulo.setOrigin({ tamano / 2, tamano / 2 });
+    circulo.setFillColor(sf::Color::Magenta);
+    circulo.setPosition(position);
+    return circulo;
+}
 
 int main()
 {
     auto window = sf::RenderWindow(sf::VideoMode({WIDTH, HEIGHT}), "wEKO");
-    window.setFramerateLimit(75);
+    window.setFramerateLimit(60);
 
-    // un rectangulo
-    sf::RectangleShape rectanguloqlo;
-    rectanguloqlo.setSize(sf::Vector2f(50, 50));
-    rectanguloqlo.setOrigin({rectanguloqlo.getSize().x / 2, rectanguloqlo.getSize().y / 2});
-    rectanguloqlo.setFillColor(sf::Color::Red);
-
-    /* obstaculos */
-    const unsigned int pared_altura = 300;
-    const unsigned int pared_anchura = 20;
-    sf::RectangleShape pared;
-    pared.setSize(sf::Vector2f(pared_anchura, pared_altura));
-    pared.setOrigin({ pared_anchura/2, pared_altura/2});
-    pared.setPosition({ WIDTH / 2, HEIGHT / 2 });
+    /* almacen de bolitas */
+    std::vector<BolitaConGravedad> bolitas;
 
     while (window.isOpen())
     {
@@ -31,23 +57,29 @@ int main()
             {
                 window.close();
             }
+            
         }
-
-        // limpia las weas anteriores
         window.clear();
 
+        /* aqui se dibujan weas */
         // obtiene posicion del mouse qlo
-        sf::Vector2i position = sf::Mouse::getPosition(window);
-
-        // DIBUJAR WEAS
-        rectanguloqlo.setPosition({(float) position.x, (float) position.y});
-        sf::Vector2f cordenadas_rectanguloqlo = rectanguloqlo.getPosition();
-
-        window.draw(rectanguloqlo);
         
-        window.draw(pared);
+        sf::Vector2f mouse_position(sf::Mouse::getPosition(window));
+        
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+            bolitas.push_back(spawnearBolita(mouse_position));
+        }
+        
+        // diguja las bolitas
+        for (BolitaConGravedad& i: bolitas) {
+            window.draw(i);
+        }
 
-        // muestra las weas que hay que mostrar
+        // encargado de la gravedad
+        for (BolitaConGravedad& i : bolitas) {
+            i.update();
+        }
+
         window.display();
     }
 }
